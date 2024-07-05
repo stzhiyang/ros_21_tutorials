bc42a8d9723d87cc68a5cd6cb70cf2476a2b7f35
stzhiyang <153494617+stzhiyang@users.noreply.github.com>
2024-07-05 10:46:25 +0800

    Update c++_class.cpp

--- linux/c++_class.cpp
+++ linux/c++_class.cpp
@@ -1,14 +1,14 @@
 /***********************************************************************
-Copyright 2020 GuYueHome (www.guyuehome.com).
+Copyright 2020 GuYueHome (www.guyuehome.com).面向对象
 ***********************************************************************/
 
 #include <iostream>
 
-class A
+class A //class 定义一个类为 A，所谓的对象
 {
 public:
-    int i;
-    void test()
+    int i;//类中定义的变量称为属性，
+    void test()////类中定义的函数称为方法，
     {
         std::cout << i << std::endl;
     }
@@ -16,10 +16,10 @@ public:
 
 int main()
 {
-    A a;
+    A a;//建立一个类A的对象为a
     
-    a.i = 10;
-    a.test();
+    a.i = 10;//对象的属性值
+    a.test();//对象的方法
     
     return 0;
 }
